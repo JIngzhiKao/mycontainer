54a75795e279ecbc86d1c890ae87e664be2a4ae1
“JIngzhiKao” <85300600+JIngzhiKao@users.noreply.github.com>
2022-06-22 14:18:56 +0800

    test

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import rhino3dm as rhino
+from flask import flask, render_template
 
 app = Flask(__name__)
 
@@ -9,3 +11,14 @@ def hello_world():
 @app.route('/test')
 def Linkstart():
     return 'Link Start!'
+
+@app.route('/news')   #增加一个news页面
+def newspage():
+    newinformation="code 2120 assessment1！"
+    return render_template("news.html",data=newinformation)
+
+app.route('/product')  #增加一个product页面
+def productpage():    
+    return render_template("product.html")
+
+
