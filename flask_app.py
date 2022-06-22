d7b304e7c065d54d83192f77f294bf00313a5985
“JIngzhiKao” <85300600+JIngzhiKao@users.noreply.github.com>
2022-06-22 17:59:54 +0800

    add static file and html

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,33 @@
-from flask import Flask
 import rhino3dm as rhino
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from test01!'
+# main
 
-@app.route('/test')
-def Linkstart():
-    return 'Link Start!'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# api
+@app.route('/api', methods=['POST'])
+def api():
+    name = request.form['name']
+    length = request.form['length']
+    width = request.form['width']
+    height = request.form['height']
+   
+ 
+    
+    msg = f'Name:{name} Length:{length} WIdth:{width} Height:{height}'
+    return jsonify({'msg': msg})
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
 
 
 
