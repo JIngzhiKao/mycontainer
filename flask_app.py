c937f7daa962bb8dbfcfef248b9e8332510b981c
“JIngzhiKao” <85300600+JIngzhiKao@users.noreply.github.com>
2022-06-29 13:56:59 +0800

    median

--- flask_app.py
+++ flask_app.py
@@ -1,34 +1,41 @@
 import rhino3dm as rhino
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+import numpy as np
 
 
+from flask import Flask
+import ghhops_server as hs
+
+# register hops app as middleware
 app = Flask(__name__)
+hops = hs.Hops(app)
 
-# main
+@hops.component(
+    "/median",
+    name="median",
+    description="find the median in this set",
+    inputs=[
+        hs.HopsNumber("set", "set", "the set to find with"),
+    ],
+    outputs=[
+        hs.HopsNumber("median", "m", "the median")
+    ]
+)
 
+def midian(set):
+    n_num = set
+    n = len(n_num) 
+    n_num.sort()
+    if n%2 == 0:
+        median_1=n_num[n//2]
+        median_2=n_num[n//2-1]
+        median = (median_1+median_2)/2
+        return(median)
+    else:
+        median = n_num[n//2]
+        return(median)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-# api
-@app.route('/api', methods=['POST'])
-def api():
-    name = request.form['name']
-    length = request.form['length']
-    width = request.form['width']
-    height = request.form['height']
-   
- 
-    
-    msg = f'Name:{name} Length:{length} WIdth:{width} Height:{height}'
-    return jsonify({'msg': msg})
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
-
-
-
+if __name__ == "__main__":
+    app.run()
 
